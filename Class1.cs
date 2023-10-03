623a7643c9f0140c97b322e493f2d365d9e011dd
MaiTheLongDraco <128013309+MaiTheLongDraco@users.noreply.github.com>
2023-10-03 20:08:49 +0700

    abc

--- Class1.cs
+++ Class1.cs
@@ -2,12 +2,9 @@
 
 public class Class1
 {
-	public Class1()
-	{
         static void Main(string[] args)
         {
             int n = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine(n);
         }
-    }
 }
